825c6bae0771b2e2578c5ab3479c9c02528c1cfe
rock-biter <74297920+rock-biter@users.noreply.github.com>
2024-05-21 23:25:32 +0200

    FIX installing dependecies

--- src/utils/index.mjs
+++ src/utils/index.mjs
@@ -4,41 +4,41 @@ import * as fs from 'fs'
 
 export default function runCommand(command, showOut = false) {
 	return new Promise((resolve, reject) => {
-		const [comm, ...args] = command.split(' ')
-		// console.log(comm, ...args)
-		const cmd = spawn(comm, args)
+		// const [comm, ...args] = command.split(' ')
+		// // console.log(comm, ...args)
+		// const cmd = spawn(comm, args)
 
-		cmd.stdout.on('data', (data) => {
-			if (showOut) console.log(`Output : ${data}`)
-			// resolve()
-		})
+		// cmd.stdout.on('data', (data) => {
+		// 	if (showOut) console.log(`Output : ${data}`)
+		// 	// resolve()
+		// })
 
-		cmd.stderr.on('data', (data) => {
-			console.error(`cmd stderr: ${data}`)
-			reject(data)
-		})
+		// cmd.stderr.on('data', (data) => {
+		// 	console.error(`cmd stderr: ${data}`)
+		// 	reject(data)
+		// })
 
-		cmd.on('close', (code) => {
-			if (code !== 0) {
-				console.log(`ps process exited with code ${code}`)
-			}
-			resolve()
-		})
-		// exec(command, (error, stdout, stderr) => {
-		// 	if (error) {
-		// 		console.error('Error', error.message)
-		// 		reject(error)
+		// cmd.on('close', (code) => {
+		// 	if (code !== 0) {
+		// 		console.log(`ps process exited with code ${code}`)
 		// 	}
-
-		// 	if (stderr) {
-		// 		console.error('Error', stderr)
-		// 		reject(stderr)
-		// 	}
-
-		// 	if (showOut) console.log(`Output : ${stdout}`)
-
 		// 	resolve()
 		// })
+		exec(command, (error, stdout, stderr) => {
+			if (error) {
+				console.error('Error', error.message)
+				reject(error)
+			}
+
+			if (stderr) {
+				console.error('Error', stderr)
+				reject(stderr)
+			}
+
+			if (showOut) console.log(`Output : ${stdout}`)
+
+			resolve()
+		})
 	})
 }
 
